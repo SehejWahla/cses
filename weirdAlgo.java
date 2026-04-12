e293538b1da6e3d5abab8d31f06302c6ca109f2f
Sehej Wahla <sehejwahla@gmail.com>
2026-04-12 10:10:03 +1000

    1068: Attempt 1, which failed on larger inputs. Maybe I should try bigger types than int??

--- weirdAlgo.java
+++ weirdAlgo.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class weirdAlgo{
 	public static void main(String[] args){
-	    System.out.printf("Hello world%n");
+	    Scanner myObj = new Scanner(System.in);
+		int n = myObj.nextInt();
+		while(n>0){
+		    System.out.printf("%d ", n);
+		    if(n==1){
+				n=0;
+			}
+			else{
+			    if(n%2==0){
+				    n /= 2;
+				}
+				else{
+				    n *= 3;
+					n++;
+				}
+			}
+		}
 	}
 }
